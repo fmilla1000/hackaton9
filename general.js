0b1dfaff18289a62f5ad6879a96b513d939c4498
fmilla1000 <fmilla1000@gmail.com>
2020-12-12 11:56:50 -0500

    arreglos

---
+++ general.js
@@ -0,0 +1,49 @@
+let z = Number(prompt("Ingrese el numero del ejercicio"))
+
+switch (z) {
+    case 1:
+        document.write('<script src="Ejercicio1.js"></script>');
+        break;
+    case 2:
+        document.write('<script src="Ejercicio2.js"></script>');
+        break;
+    case 3:
+        document.write('<script src="Ejercicio3.js"></script>');
+        break;
+    case 4:
+        document.write('<script src="Ejercicio4.js"></script>');
+        break;
+    case 5:
+        document.write('<script src="Ejercicio5.js"></script>');
+        break;
+    case 6:
+        document.write('<script src="Ejercicio6.js"></script>');
+        break;
+    case 7:
+        document.write('<script src="Ejercicio7.js"></script>');
+        break;
+    case 8:
+        document.write('<script src="Ejercicio8.js"></script>');
+        break;
+    case 9:
+        document.write('<script src="Ejercicio9.js"></script>');
+        break;
+    case 10:
+        document.write('<script src="Ejercicio10.js"></script>');
+        break;
+    case 11:
+        document.write('<script src="Ejercicio11.js"></script>');
+        break;
+    case 12:
+        document.write('<script src="Ejercicio12.js"></script>');
+        break;
+    case 13:
+        document.write('<script src="Ejercicio13.js"></script>');
+        break;
+    case 14:
+        document.write('<script src="Ejercicio14.js"></script>');
+        break;
+    case 15:
+        document.write('<script src="Ejercicio15.js"></script>');
+        break;
+}
